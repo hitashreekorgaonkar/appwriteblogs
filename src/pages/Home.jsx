67b5ba9cbd7ab12605a5f07801d68e1cb8c0313e
hitashreekorgaonkar <hkorgaonkar10@gmail.com>
2023-11-01 18:59:26 +0530

    fieldId issue solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
 import { Container, PostCard } from "../components";
 
-function Home() {
+const Home = () => {
   const [posts, setPosts] = useState([]);
-
   useEffect(() => {
     appwriteService.getPosts().then((posts) => {
       if (posts) {
@@ -35,59 +34,13 @@ function Home() {
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
               <PostCard {...post} />
+              {/* <PostCard post={post} /> both ways are ok */}
             </div>
           ))}
         </div>
       </Container>
     </div>
   );
-}
+};
 
 export default Home;
-
-// import React, { useEffect, useState } from "react";
-// import appwriteService from "../appwrite/config";
-// import { Container, PostCard } from "../components";
-
-// const Home = () => {
-//   const [posts, setPosts] = useState([]);
-//   useEffect(() => {
-//     appwriteService.getPosts().then((posts) => {
-//       if (posts) {
-//         setPosts(posts.documents);
-//       }
-//     });
-//   }, []);
-
-//   if (posts.length === 0) {
-//     return (
-//       <div className="w-full py-8 mt-4 text-center">
-//         <Container>
-//           <div className="flex flex-wrap">
-//             <div className="p-2 w-full">
-//               <h1 className="text-2xl font-bold hover:text-gray-500">
-//                 Login to read posts
-//               </h1>
-//             </div>
-//           </div>
-//         </Container>
-//       </div>
-//     );
-//   }
-//   return (
-//     <div className="w-full py-8">
-//       <Container>
-//         <div className="flex flex-wrap">
-//           {posts.map((post) => (
-//             <div key={post.$id} className="p-2 w-1/4">
-//               <PostCard {...post} />
-//               {/* <PostCard post={post} /> both ways are ok */}
-//             </div>
-//           ))}
-//         </div>
-//       </Container>
-//     </div>
-//   );
-// };
-
-// export default Home;
