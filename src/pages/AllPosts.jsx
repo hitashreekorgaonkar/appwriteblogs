cad0a401589443537ef5747e26864dea4a947541
hitashreekorgaonkar <hkorgaonkar10@gmail.com>
2023-11-01 18:52:02 +0530

    fieldId issue solved

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, PostCard } from "../components";
 import appwriteService from "../appwrite/config";
 
-const AllPosts = () => {
+function AllPosts() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {}, []);
   appwriteService.getPosts([]).then((posts) => {
@@ -16,13 +16,42 @@ const AllPosts = () => {
         <div className="flex flex-wrap">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
-              <PostCard post={...post} />
+              <PostCard {...post} />
             </div>
           ))}
         </div>
       </Container>
     </div>
   );
-};
+}
 
 export default AllPosts;
+
+// import React, { useState, useEffect } from "react";
+// import { Container, PostCard } from "../components";
+// import appwriteService from "../appwrite/config";
+
+// const AllPosts = () => {
+//   const [posts, setPosts] = useState([]);
+//   useEffect(() => {}, []);
+//   appwriteService.getPosts([]).then((posts) => {
+//     if (posts) {
+//       setPosts(posts.documents);
+//     }
+//   });
+//   return (
+//     <div className="w-full py-8">
+//       <Container>
+//         <div className="flex flex-wrap">
+//           {posts.map((post) => (
+//             <div key={post.$id} className="p-2 w-1/4">
+//               <PostCard post={...post} />
+//             </div>
+//           ))}
+//         </div>
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default AllPosts;
