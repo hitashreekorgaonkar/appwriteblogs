482cdf46f2986411b78d374b4d3d92342f62923a
hitashreekorgaonkar <hkorgaonkar10@gmail.com>
2023-11-01 19:58:43 +0530

    fieldId issue solved

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,16 +1,16 @@
 import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
-import { Button, Input, RTE, Select } from "..";
+import { Button, Input, Select, RTE } from "../index";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-export default function PostForm({ post }) {
+const PostForm = ({ post }) => {
   const { register, handleSubmit, watch, setValue, control, getValues } =
     useForm({
       defaultValues: {
         title: post?.title || "",
-        slug: post?.$id || "",
+        slug: post?.slug || "",
         content: post?.content || "",
         status: post?.status || "active",
       },
@@ -19,7 +19,6 @@ export default function PostForm({ post }) {
   const navigate = useNavigate();
   const userData = useSelector((state) => state.auth.userData);
   console.log("1 userData", userData);
-
   const submit = async (data) => {
     if (post) {
       const file = data.image[0]
@@ -39,8 +38,9 @@ export default function PostForm({ post }) {
         navigate(`/post/${dbPost.$id}`);
       }
     } else {
+      //TODO: 27:00 min video 25
       const file = await appwriteService.uploadFile(data.image[0]);
-      console.log("1 file", file);
+
       if (file) {
         const fileId = file.$id;
         data.featuredImage = fileId;
@@ -48,19 +48,23 @@ export default function PostForm({ post }) {
           ...data,
           userId: userData.$id,
         });
+      }
 
-        if (dbPost) {
-          navigate(`/post/${dbPost.$id}`);
-        }
+      if (dbPost) {
+        navigate(`/post/${dbPost.$id}`);
       }
     }
   };
 
   const slugTransform = useCallback((value) => {
     if (value && typeof value === "string")
+      // const slug = value.toLowerCase().replace(/ /g, "_");
+      // setValue("slug", slug);
+      // return slug;
+
       return value
         .trim()
-        .toLowerCase()
+        .toLocaleLowerCase()
         .replace(/[^a-zA-Z\d\s]+/g, "-")
         .replace(/\s/g, "-");
 
@@ -70,11 +74,12 @@ export default function PostForm({ post }) {
   React.useEffect(() => {
     const subscription = watch((value, { name }) => {
       if (name === "title") {
-        setValue("slug", slugTransform(value.title), { shouldValidate: true });
+        setValue("slug", slugTransform(value.title, { shouldValidate: true }));
       }
     });
-
-    return () => subscription.unsubscribe();
+    return () => {
+      subscription.unsubscribe();
+    };
   }, [watch, slugTransform, setValue]);
 
   return (
@@ -137,153 +142,6 @@ export default function PostForm({ post }) {
       </div>
     </form>
   );
-}
+};
 
-// import React, { useCallback } from "react";
-// import { useForm } from "react-hook-form";
-// import { Button, Input, Select, RTE } from "../index";
-// import appwriteService from "../../appwrite/config";
-// import { useNavigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
-
-// const PostForm = ({ post }) => {
-//   const { register, handleSubmit, watch, setValue, control, getValues } =
-//     useForm({
-//       defaultValues: {
-//         title: post?.title || "",
-//         slug: post?.slug || "",
-//         content: post?.content || "",
-//         status: post?.status || "active",
-//       },
-//     });
-
-//   const navigate = useNavigate();
-//   const userData = useSelector((state) => state.auth.userData);
-//   console.log("1 userData", userData);
-//   const submit = async (data) => {
-//     if (post) {
-//       const file = data.image[0]
-//         ? await appwriteService.uploadFile(data.image[0])
-//         : null;
-
-//       if (file) {
-//         appwriteService.deleteFile(post.featuredImage);
-//       }
-
-//       const dbPost = await appwriteService.updatePost(post.$id, {
-//         ...data,
-//         featuredImage: file ? file.$id : undefined,
-//       });
-
-//       if (dbPost) {
-//         navigate(`/post/${dbPost.$id}`);
-//       }
-//     } else {
-//       //TODO: 27:00 min video 25
-//       const file = await appwriteService.uploadFile(data.image[0]);
-//       console.log("2 file", file);
-
-//       if (file) {
-//         const fileId = file.$id;
-//         data.featuredImage = fileId;
-//         const dbPost = await appwriteService.createPost({
-//           ...data,
-//           userId: userData.$id,
-//         });
-
-//         if (dbPost) {
-//           navigate(`/post/${dbPost.$id}`);
-//         }
-//       }
-//     }
-//   };
-
-//   const slugTransform = useCallback((value) => {
-//     if (value && typeof value === "string")
-//       // const slug = value.toLowerCase().replace(/ /g, "_");
-//       // setValue("slug", slug);
-//       // return slug;
-
-//       return value
-//         .trim()
-//         .toLocaleLowerCase()
-//         .replace(/[^a-zA-Z\d\s]+/g, "-")
-//         .replace(/\s/g, "-");
-
-//     return "";
-//   }, []);
-
-//   React.useEffect(() => {
-//     const subscription = watch((value, { name }) => {
-//       if (name === "title") {
-//         setValue("slug", slugTransform(value.title, { shouldValidate: true }));
-//       }
-//     });
-//     return () => {
-//       subscription.unsubscribe();
-//     };
-//   }, [watch, slugTransform, setValue]);
-
-//   return (
-//     <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-//       <div className="w-2/3 px-2">
-//         <Input
-//           label="Title :"
-//           placeholder="Title"
-//           className="mb-4"
-//           {...register("title", { required: true })}
-//         />
-//         <Input
-//           label="Slug :"
-//           placeholder="Slug"
-//           className="mb-4"
-//           {...register("slug", { required: true })}
-//           onInput={(e) => {
-//             setValue("slug", slugTransform(e.currentTarget.value), {
-//               shouldValidate: true,
-//             });
-//           }}
-//         />
-//         <RTE
-//           label="Content :"
-//           name="content"
-//           control={control}
-//           defaultValue={getValues("content")}
-//         />
-//       </div>
-//       <div className="w-1/3 px-2">
-//         <Input
-//           label="Featured Image :"
-//           type="file"
-//           className="mb-4"
-//           accept="image/png, image/jpg, image/jpeg, image/gif"
-//           {...register("image", { required: !post })}
-//         />
-//         {post && (
-//           <div className="w-full mb-4">
-//             <img
-//               src={appwriteService.getFilePreview(post.featuredImage)}
-//               alt={post.title}
-//               className="rounded-lg"
-//             />
-//           </div>
-//         )}
-//         <Select
-//           options={["active", "inactive"]}
-//           label="Status"
-//           className="mb-4"
-//           {...register("status", { required: true })}
-//         />
-//         <Button
-//           type="submit"
-//           bgColor={post ? "bg-green-500" : undefined}
-//           className="w-full"
-//         >
-//           {post ? "Update" : "Submit"}
-//         </Button>
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default PostForm;
+export default PostForm;
--- src/store/store.js
+++ src/store/store.js
@@ -4,21 +4,9 @@ import authSlice from "./authSlice";
 const store = configureStore({
   reducer: {
     auth: authSlice,
-    //TODO: add more slices here for posts
+    // post: postSlice,
+    //TODO: add more slices here for posts 17:00 ep 27
   },
 });
 
 export default store;
-
-// import { configureStore } from "@reduxjs/toolkit";
-// import authSlice from "./authSlice";
-
-// const store = configureStore({
-//   reducer: {
-//     auth: authSlice,
-//     // post: postSlice,
-//     //TODO: add more slices here for posts 17:00 ep 27
-//   },
-// });
-
-// export default store;
